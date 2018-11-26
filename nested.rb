10e3efbc5498775c3961e7d7b8e84ad7d1587231
Matteo Muscas <thatman.j@gmail.com>
2018-11-26 15:21:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
+programmer_hash[:yukihiro_matsumoto][:known_for]
+programmer_hash[:yukihiro_matsumoto][:languages]
 
 end
 
